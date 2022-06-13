f6f9cec3778bd5231801ed99f4e2dc5f87d55d42
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-13 15:59:11 +0200

    Create database query

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row row-cols-3">
+            @forelse($movies as $movie)
+            <div class="col">
+                <div class="card">
+                    <h3>{{$movie->original_title}}</h3>
+                    <p>{{$movie->nationality}}</p>
+                    <p>{{$movie->date}}</p>
+                    <p>{{$movie->vote}}</p>
+                </div>
+            </div>
+            <!-- /.col -->
+            @empty
+            <p>Nothing to show!</p>
+            @endforelse
+        </div>
+        <!-- /.row row-cols-3 -->
+    </div>
+    <!-- /.container -->
+@endsection
